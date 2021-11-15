0e579c43338af70c48324af8a38604b6f1718125
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-11-15 23:26:45 +0200

    Service

--- user-service/src/main/java/com/intentsg/service/user/service/ServiceClass.java
+++ user-service/src/main/java/com/intentsg/service/user/service/ServiceClass.java
@@ -1,12 +1,49 @@
 package com.intentsg.service.user.service;
 
 import com.intentsg.service.user.repository.UserRepository;
+import com.intentsg.service.user.userdto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceClass {
     @Autowired
     UserRepository userRepository;
+
+
+    public List<UserDto> findAllUsers(){
+        List<UserDto> list=userRepository.findAll().stream()
+                .map(e->{
+                    UserDto userDto= new UserDto();
+                    userDto.setUserNameDto(e.getFirstName());
+                    userDto.setUserIdDto(e.getUserId());
+                    return userDto;
+                }).collect(Collectors.toList());
+        return list;
+    }
+    public List<UserDto> getUsersByByUserId(Long userIdDto){
+        List<UserDto> dtoList=userRepository.getUserByUserId(userIdDto).stream()
+                .map(e->{
+                    UserDto userDto=new UserDto();
+                    userDto.getUserIdDto();
+                    userDto.getUserIdDto();
+                    return userDto;
+                }).collect(Collectors.toList());
+        return dtoList;
+    }
+    public List<UserDto> findAllByFirstName(String  userNameDto){
+        List<UserDto> list = userRepository.findAllByFirstName(userNameDto).stream()
+                .map(e->{
+                    UserDto userDto = new UserDto() ;
+                    userDto.getUserNameDto();
+                    userDto.setUserNameDto(e.getFirstName());
+                    return userDto;
+                }).collect(Collectors.toList());
+        return list;
+    }
 
 }
--- user-service/src/main/java/com/intentsg/service/user/userdto/UserDto.java
+++ user-service/src/main/java/com/intentsg/service/user/userdto/UserDto.java
@@ -1,4 +1,9 @@
 package com.intentsg.service.user.userdto;
 
+import lombok.Data;
+@Data
 public class UserDto {
+        private Long userIdDto;
+        private String userNameDto;
+
 }
